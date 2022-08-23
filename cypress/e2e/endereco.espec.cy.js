24dd46726632bb240ae836ffb67c524845d45c41
victorleao <leaovic@icloud.com>
2022-08-22 22:33:17 -0300

    manipulando arquivos com massa de dados

--- cypress/e2e/endereco.espec.cy.js
+++ cypress/e2e/endereco.espec.cy.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress"/>
 import EnderecoPage from '../support/page-objects/endereco.page';
+import enderecos from '../fixtures/enderecos.json'
 
 describe('Fuincionalidade Endereços - Faturamento e Entrega', () => {
 	beforeEach(() => {
@@ -10,7 +11,7 @@ describe('Fuincionalidade Endereços - Faturamento e Entrega', () => {
 		// cy.login('aluno35@teste.com', 'teste@teste123') foi passado na forma supracitada
 	});
 
-	it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+	it('Deve fazer cadastro de faturamento com sucesso', () => {
 		EnderecoPage.editarEnderecoFaturamento(
 			'Victor',
 			'Leao',
@@ -23,6 +24,23 @@ describe('Fuincionalidade Endereços - Faturamento e Entrega', () => {
 			'24412000',
 			'8199998888',
 			'teste@teste.com'
+		);
+		cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.');
+	});
+
+    it.only('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de Massa de Dados', () => {
+		EnderecoPage.editarEnderecoFaturamento(
+			enderecos[1].nome,
+            enderecos[1].sobrenome,
+            enderecos[1].empresa,
+            enderecos[1].pais,
+            enderecos[1].endereco,
+            enderecos[1].complemento,
+            enderecos[1].cidade,
+            enderecos[1].estado,
+            enderecos[1].cep,
+            enderecos[1].telefone,
+            enderecos[1].email
 		);
 		cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.');
 	});
